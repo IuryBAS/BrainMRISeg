c6c81c9492c54b8b67b7d75f45b2fc1a87c51e9e
Iury Andrade <iurybas@gmail.com>
2022-07-04 11:13:56 -0300

    Adicao de casos de teste em batch e arquivo para execucao dos testes watershed

---
+++ src/args_test_watershed.py
@@ -0,0 +1,62 @@
+from skimage.filters import rank
+from skimage.morphology import disk, square, diamond, rectangle
+from skimage.morphology import erosion, dilation, opening, closing
+
+CASE_1 = {'subject_id': '0018',
+          'slice_range': (173, 182),
+          'perspective': 'a',
+          'norm_thres': None,
+          'apply_morphy': {'denoise_1': {'method': rank.median, 'se': disk(2)},
+                           'gradient_1': {'method': rank.gradient, 'se': disk(1)}
+                           },
+          'x': 116,
+          'y': 85,
+          'outer_mark_x': 109,
+          'outer_mark_y': 99
+          }
+
+CASE_2 = {'subject_id': '0317',
+          'slice_range': (173, 182),
+          'perspective': 'a',
+          'norm_thres': 19,
+          'apply_morphy': {'denoise_1': {'method': rank.mean, 'se': disk(1)},
+                           'closing_1': {'method': closing, 'se': rectangle(5,1)},
+                           'closing_2': {'method': closing, 'se': disk(1)},
+                           'gradient_1': {'method': rank.gradient, 'se': disk(1)}
+                           },
+          'x': 155,
+          'y': 103,
+          'outer_mark_x': 109,
+          'outer_mark_y': 99
+          }
+
+CASE_3 = {'subject_id': '0023',
+          'slice_range': (165, 172),
+          'perspective': 'a',
+          'norm_thres': 20,
+          'apply_morphy': {'denoise_1': {'method': rank.mean, 'se': disk(1)},
+                           'gradient_1': {'method': rank.gradient, 'se': disk(1)},
+                           'closing_1': {'method': closing, 'se': square(1)},
+                           },
+          'x': 143,
+          'y': 143,
+          'outer_mark_x': 109,
+          'outer_mark_y': 99
+          }
+
+CASE_4 = {'subject_id': '0035',
+          'slice_range': (114, 120),
+          'perspective': 'a',
+          'norm_thres': 50,
+          'apply_morphy': {'denoise_1': {'method': rank.median, 'se': disk(2)},
+                           'closing_1': {'method': closing, 'se': disk(7)},
+                           'gradient_1': {'method': rank.gradient, 'se': disk(1)},
+                           },
+          'x': 166,
+          'y': 100,
+          'outer_mark_x': 109,
+          'outer_mark_y': 99
+          }
+
+
+TESTE_CASES = [CASE_1, CASE_2, CASE_3, CASE_4]
---
+++ src/run_segmentation.py
@@ -0,0 +1,78 @@
+import watershed
+import numpy as np
+import utils
+import matplotlib.pyplot as plt
+import math
+import args_test_watershed
+
+def plot_mosaic(slices_array, slice_range):
+
+    start_slice, end_slice = slice_range
+    n_cols = 4
+    n_rows = math.ceil((end_slice - start_slice) / n_cols)
+    fig, axs = plt.subplots(nrows=n_rows, ncols=n_cols)
+
+    for i, slice_sub in enumerate(slices_array):
+        ax = plt.subplot(n_rows, n_cols, i+1)
+        plt.imshow(slice_sub, cmap='gray')
+    
+
+
+def get_batch_data(df, subject_id, slices_range, perspective, masks=False):
+    start_slice, end_slice = slices_range
+    slices_array = []
+    masks_array = []
+    for n_slice in range(start_slice, end_slice):
+        slice_n = utils.get_image(df, subject_id, n_slice, perspective, masks)
+        slices_array.append(np.flip(slice_n))
+    
+    return slices_array
+
+
+def apply_batch_watershed(df, subject_id, slices_range, perspective, norm_thres,
+                          morph_args, x, y, outer_x, outer_y):
+
+    slices_array = get_batch_data(df, subject_id, slices_range, perspective)
+    masks_array = get_batch_data(df, subject_id, slices_range, perspective, True)
+    mask = [[x, y], [outer_x, outer_y]]
+    seg_masks_array = []
+    for slice in slices_array:
+        
+        mri_norm = utils.normalize_img(slice)
+        mri_thresholded = mri_norm.copy()
+
+        if norm_thres:
+           mri_thresholded[mri_thresholded >= norm_thres] = 255
+
+        mri_denoised = watershed.apply_morphology(mri_thresholded, **morph_args)
+        img_seg = watershed.apply_watershed(mri_denoised, mask)
+        img_seg[img_seg != 2] = 0
+        seg_masks_array.append(img_seg)
+
+        
+
+    plot_mosaic(seg_masks_array, slices_range)
+    plot_mosaic(masks_array, slices_range)
+    plt.show()
+
+
+if __name__ == '__main__':
+
+    df = utils.build_dataframe('../Episurg', 'subjects.csv')
+    
+    CASES = args_test_watershed.TESTE_CASES
+
+    for case_test in CASES:
+        sub_id = case_test['subject_id']
+        slices_range = case_test['slice_range']
+        perspective = case_test['perspective']
+        norm_thres = case_test['norm_thres']
+        morphy = case_test['apply_morphy']
+        x = case_test['x']
+        y = case_test['y']
+        outer_x = case_test['outer_mark_x']
+        outer_y = case_test['outer_mark_y']
+        apply_batch_watershed(df, sub_id, slices_range, perspective, norm_thres,
+                              morphy, x, y, outer_x, outer_y)
+
+
