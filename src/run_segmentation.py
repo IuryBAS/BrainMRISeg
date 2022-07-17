7eaf5797c2071f6fc62d2a2fc9cd799218100dc8
Iury Andrade <iurybas@gmail.com>
2022-07-17 01:18:53 -0300

    Adequacao do run_segmentation para exibicao do mosaico completo com groundtruth

--- src/run_segmentation.py
+++ src/run_segmentation.py
@@ -12,44 +12,42 @@ def plot_mosaic(slices_array, slice_range, title):
     Function to plot (or save) a mosaic with all slices masks
 
     Params:
-        slices_array: Array with all MRI images slices, segmentatino masks 
-                      and groundtruth masks to be plotted
+        slices_array: Array with all slices masks to be plotted
         slice_range: Range of slices to calculate the grid of the mosaic
         title: String title of the mosaic plot
     '''
-    # From the array of arrays slices_array, containing all three group of slices, 
-    # get the mri_slices, segmentations_masks and groundtruth masks
     mri_slice = slices_array[0]
-    seg_mask_array = slices_array[1]
-    gt_mask_array = slices_array[2]
+    seg_water_mask_array = slices_array[1]
+    seg_cv_mask_array = slices_array[2]
+    gt_mask_array = slices_array[3]
 
     start_slice, end_slice = slice_range
-    # Set size of the mosaic plot to 3 columns, for mri image and both masks
-    n_cols = 3
-    n_rows = math.ceil((end_slice - start_slice))
-    fig, axs = plt.subplots(nrows=n_rows, ncols=n_cols, figsize=(20, 50))
+    n_cols = 4
+    n_rows = math.ceil((end_slice - start_slice)) # / n_cols)
+    fig, axs = plt.subplots(nrows=n_rows, ncols=n_cols, figsize=(20, 45))
     fig.suptitle(title)
     i = 0
     slice_count = 0
-    # Make the automated plotting of the images. First the original mri image,
-    # followed by the segmentation masks and the groundtruth masks overlayed
-    for mri, seg, gt in zip(mri_slice, seg_mask_array, gt_mask_array):
+    for mri, seg, chan, gt in zip(mri_slice, seg_water_mask_array, seg_cv_mask_array, gt_mask_array):
         ax = plt.subplot(n_rows, n_cols, i+1)
         ax.title.set_text(f'Slice {start_slice + slice_count} - MRI')
         plt.imshow(mri, cmap='gray')
         ax = plt.subplot(n_rows, n_cols, i+2)
-        ax.title.set_text(f'Slice {start_slice + slice_count} - Segmentation')
+        ax.title.set_text(f'Slice {start_slice + slice_count} - Watershed')
         plt.imshow(mri, cmap='gray')
         plt.imshow(seg, cmap='inferno', alpha=0.7)
         ax = plt.subplot(n_rows, n_cols, i+3)
+        ax.title.set_text(f'Slice {start_slice + slice_count} - Chan-Vese')
+        plt.imshow(mri, cmap='gray')
+        plt.imshow(chan, cmap='inferno', alpha=0.7)
+        ax = plt.subplot(n_rows, n_cols, i+4)
         ax.title.set_text(f'Slice {start_slice + slice_count} - GT')
         plt.imshow(mri, cmap='gray')
         plt.imshow(gt, cmap='inferno', alpha=0.7)
-        i += 3
+        i += 4
         slice_count += 1
-
-    # Persist the mosaic plot in a image file
     plt.savefig(title)
+
 
 
 def get_batch_data(df, subject_id, slices_range, perspective, masks=False):
@@ -106,10 +104,6 @@ def apply_batch_watershed(df, subject_id, slices_range, perspective, norm_thres,
     '''
     # Get the mri images slices from the desired subject
     slices_array = get_batch_data(df, subject_id, slices_range, perspective)
-    # Get the groundtruth masks from the slices examinated
-    masks_array = get_batch_data(df, subject_id, slices_range, perspective, True)
-    # Array to store the normalized mri images a posteriori
-    mri_norm_array = []
     # Set the masks for use in the watershed method: for the (x, y) coordinate
     # of the region of interest and the (x, y) for the outer region inside the
     # brain, but out of the region of interest. In total, theses masks are used
@@ -118,9 +112,6 @@ def apply_batch_watershed(df, subject_id, slices_range, perspective, norm_thres,
     mask = [[x, y], [outer_x, outer_y]]
     # Array to store the segmentation masks results
     seg_masks_array = []
-    # Array to store the intermediary preprocessed results. This array is for 
-    # inspecion purposes only
-    preprocessed_mri = []
     # Array to store the dices_scores for each slice mask
     dice_scores = []
     
@@ -148,16 +139,10 @@ def apply_batch_watershed(df, subject_id, slices_range, perspective, norm_thres,
         # Store the dices scores, normalized mri slices, preprocesses slices and
         # and the segmented masks results
         dice_scores.append(dice_score)
-        mri_norm_array.append(mri_norm)
-        preprocessed_mri.append(mri_denoised)
         seg_masks_array.append(img_seg)
 
-    # Create a unified array for display pruporses
-    resulting_slices = [mri_norm_array, seg_masks_array, masks_array]
-    # Call the building mosaic plot function
-    plot_mosaic(resulting_slices, slices_range, f'CASE {n_case} - Results')
 
-    return (resulting_slices, preprocessed_mri), np.asarray(dice_scores)
+    return seg_masks_array, np.asarray(dice_scores)
 
 
 def apply_batch_chan_vese(df, subject_id, slices_range, perspective, mu, 
@@ -175,9 +160,8 @@ def apply_batch_chan_vese(df, subject_id, slices_range, perspective, mu,
         dice_scores.append(dice_score)
         seg_masks_array.append(img_seg)
 
-    plot_mosaic(seg_masks_array, slices_range, f'CASE {n_case} - Chan Vese Segmented Masks')
 
-    return np.asarray(dice_scores)
+    return seg_masks_array, np.asarray(dice_scores)
 
 
 def dice_score_similarity(prev_seg, bg_mask, value_mask=1):
@@ -202,14 +186,16 @@ def dice_score_similarity(prev_seg, bg_mask, value_mask=1):
 
 if __name__ == '__main__':
 
+    # Load the local Episurg dataset
     df = utils.build_dataframe('../Episurg', 'subjects.csv')
-
+    # Load the cases from the args_test file
     CASES = args_test.TESTE_CASES
     dices_scores_watershed = []
     dices_scores_chan_vese = []
     n_case = 1
     res_dict = {}
     for case_test in CASES:
+        # Get the params for the current case
         sub_id = case_test['subject_id']
         slices_range = case_test['slice_range']
         perspective = case_test['perspective']
@@ -222,26 +208,60 @@ if __name__ == '__main__':
         mu = case_test['mu']
         lambda1 = case_test['lambda1']
         lambda2 = case_test['lambda2']
-        dices_chan = apply_batch_chan_vese(df, sub_id, slices_range, perspective,
+
+        # Print some information about the current case being executed
+        print('========================================')
+        print(f'         EXECUTANDO CASO {n_case}')
+        print('==========Parametros gerais=============')
+        print(f'Id do Subject: {sub_id}')
+        print(f'Intervalo de slices: {slices_range}')
+        print(f'Perspectiva dos slices: {perspective}')
+        print(f'Coordanada X da semente: {x}')
+        print(f'Coordanada Y da semente: {y}')
+        print('\n==========Parametros Watershed=============')
+        print(f'Threshold de pré-processamento: {norm_thres}')
+        print(f'Pré-processamentos definidos e seus respectivos parametros:')
+        for key in morphy:
+            print(f'   Pré-processamento: {key}')
+            print(f'   Parametros:\n    {morphy[key]}')
+
+        print(f'Coordanada X da semente externa: {outer_x}')
+        print(f'Coordanada Y da semente: {outer_y}')
+
+        print('\n==========Parametros Chan Vese=============')
+        print(f'Mu: {mu}')
+        print(f'Lambda1: {lambda1}')
+        print(f'Lambda2: {lambda2}')
+        print(f'\n\nExecutando métodos de segmentação. Aguarde, pode levar alguns segundos ou até minutos...')
+        # Get the mri images slices from the desired subject
+        slices_array = get_batch_data(df, sub_id, slices_range, perspective)
+        mri_slices = []
+        for slices in slices_array:
+            mri_slices.append(utils.normalize_img(slices))
+        # Get the groundtruth masks from the slices examinated
+        masks_array = get_batch_data(df, sub_id, slices_range, perspective, True)
+        # Perform the chan vese segmentation in the slices batch
+        masks_chan_vese, dices_chan = apply_batch_chan_vese(df, sub_id, slices_range, perspective,
                                            mu, lambda1, lambda2, x, y, n_case)
-        _, dices_water = apply_batch_watershed(df, sub_id, slices_range,
+        # Perform the watershed segmentation in the slices batch
+        masks_watershed, dices_water = apply_batch_watershed(df, sub_id, slices_range,
                                             perspective, norm_thres, morphy, x,
                                             y, outer_x, outer_y, n_case)
-        
-        dices_scores_chan_vese.append(dices_chan)
-        dices_scores_watershed.append(dices_water)
-        res_dict[f'CASE{n_case}'] = {'chan_vese': dices_chan, 
-                                     'watershed': dices_water}
+        # Create the resulting slices array for plotting the mri image and masks
+        resulting_slices = [mri_slices, masks_watershed, masks_chan_vese, masks_array]
+        # Create the mosaic plot with the mri image and segmentations and groundtruth masks
+        plot_mosaic(resulting_slices, slices_range, f'CASO {n_case} - Resultados Watershed X Chan-Vese X Groundtruth')
 
+        # Print the dice scores for both watershed mask and chan-vese mask for the current case slices
+        i = 0
+        s_start, s_end = slices_range
+        print(f'====================: Dice Score dos métodos')
+        print(f'====================: Watershed  || Chan Vese')
+        for s in range(s_start, s_end):
+            print(f'Slice {s} dice score:    {dices_water[i]:0.4f}  ||    {dices_chan[i]:0.4f}')
+            i += 1
+        print(f'\n============== Média:    {dices_water.mean():0.4f}  ||    {dices_chan.mean():0.4f}')
+        print(f'\nCaso {n_case} concluído. Figura com gráficos salva no diretório\n\n\n')
+        plt.show()
         n_case += 1
-        #plt.show()
-    f = open('dict.txt', 'w')
-    f.write(str(res_dict))
-    f.close()
-    '''
-    i = 1
-    for score_chan, score_water in zip(dices_scores_chan_vese, dices_scores_watershed):
-        print('Mean water for case {}: {}'.format(i, np.mean(score_water)))
-        print('Mean chan for case {}: {}'.format(i, np.mean(score_chan)))
-        i += 1
-    '''
+
